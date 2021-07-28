d2a2be55cfaf77aea7336feab0a98f37eb570487
gnrfilipe04 <gnr_filipe@hotmail.com>
2021-07-27 22:06:43 -0300

    :sparkles: Docker is running

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
+import "./database";
 
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
@@ -11,4 +12,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running!!"));
+app.listen(3333, () => console.log("Server is running !!!"));
